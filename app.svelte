1fbd31e9fbe4e9b59cf3e9060dbeaf2c672bb129
Raymond <raymond.luke.spartz@gmail.com>
2022-07-05 20:06:38 -0700

    added app.svelte

---
+++ app.svelte
@@ -0,0 +1,57 @@
+<script>
+	let name = 'Article Title From Database';
+</script>
+
+<body>
+	<article-image>
+		
+		
+		
+	</article-image>
+  <header> 
+		<h1>This is {name}</h1>
+	</header>
+	
+
+
+
+</body>
+
+
+<style>
+	
+
+	article-image {
+		width: 100%;
+		height: 66vh;
+		background-color: #1111;		
+	}
+	header {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		width: 90%;
+	  height: 200px;
+		background-color: #2222;	
+		margin: -10vh;
+		border-radius: 2em;
+	}	
+	h1 {
+	  font-size: 2.7em;
+		font-family: Helvetica;
+	}
+	body {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+</style>
+
+
+
+
+
+
+
+
+
